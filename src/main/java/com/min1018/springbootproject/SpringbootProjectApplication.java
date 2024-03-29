b0c65841c480117e78e864ebc8f1488df5049c16
min1018 <102647148+min1018@users.noreply.github.com>
2024-03-30 01:02:40 +0900

    initial commit

---
+++ src/main/java/com/min1018/springbootproject/SpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.min1018.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/min1018/springbootproject/SpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min1018.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
