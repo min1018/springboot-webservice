6ed2ee04bc41d92385ed8b875b04aa74daba4e67
min1018 <102647148+min1018@users.noreply.github.com>
2024-04-01 20:36:44 +0900

    Test: 추가 Posts 테스트 코드

---
+++ src/test/java/com/min1018/springbootproject/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.min1018.springbootproject.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After(value = "execution(* com.test.controller.TestController.*(..))")
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("meet214ba@naver.com")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
