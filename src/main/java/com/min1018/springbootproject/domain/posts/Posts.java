16335a97956b77da758134c94e68c2dba50423c0
min1018 <102647148+min1018@users.noreply.github.com>
2024-04-02 09:53:43 +0900

    Feat: enable JPA Auditing

--- src/main/java/com/min1018/springbootproject/domain/posts/Posts.java
+++ src/main/java/com/min1018/springbootproject/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.min1018.springbootproject.domain.posts;
 
+import com.min1018.springbootproject.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
