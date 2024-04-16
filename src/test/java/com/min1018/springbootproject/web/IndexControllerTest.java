a4a22006aa2546cfe0dea7cbcbcdc33a88e7fda6
min1018 <102647148+min1018@users.noreply.github.com>
2024-04-16 19:42:51 +0900

    Test: view for crreating posts

---
+++ src/test/java/com/min1018/springbootproject/web/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package com.min1018.springbootproject.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+        //then
+        assertThat(body).contains("spring boot webservice");
+    }
+}
