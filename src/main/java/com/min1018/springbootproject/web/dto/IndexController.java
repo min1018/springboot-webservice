d998475d4e93991671b9888e6f7422d8e087fe45
min1018 <102647148+min1018@users.noreply.github.com>
2024-04-16 19:39:53 +0900

    Feat: view page for create

---
+++ src/main/java/com/min1018/springbootproject/web/dto/IndexController.java
@@ -0,0 +1,20 @@
+package com.min1018.springbootproject.web.dto;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
